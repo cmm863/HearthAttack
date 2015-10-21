1279a16ba2ae55535a30c968807aaad68988edd2
Connor McBride <cmm863@mst.edu>
2015-10-21 13:05:37 -0500

    Added all hs cards json for reference. Also, correctly tracking cards that are drawn/mulliganned and played.

--- parser.py
+++ parser.py
@@ -22,13 +22,13 @@ for line in lines:
     if line[0] == '2':
         double = True
         line = line[2:]
-    card = card_pb2.Card()
-    card.has_been_used = False
-    card.in_hand = False
-    card.name = line.strip()
-    deck.cards.extend([card])
+    temp_card = card_pb2.Card()
+    temp_card.has_been_used = False
+    temp_card.in_hand = False
+    temp_card.name = line.strip()
+    deck.cards.extend([temp_card])
     if double:
-        deck.cards.extend([card])
+        deck.cards.extend([temp_card])
 
 
 player_model = player_model_pb2.PlayerModel()
@@ -38,28 +38,50 @@ player_model.hero.CopyFrom(hero)
 player_model.deck.CopyFrom(deck)
 player_model.max_mana = 1
 
-for card in player_model.deck.cards:
-    print(card.name)
 
-'''
 logfile = open("/Users/connor/Library/Logs/Unity/Player.log", 'r')
 loglines = follow(logfile)
 for line in loglines:
     if "TRANSITIONING" in line:
-        if "to FRIENDLY HAND" in line:             # Card drawn
-            print("I drew: " + parseName(line))
+        ## Draw Card
+        if "to FRIENDLY HAND" in line:
+            card_name = parseName(line)             # Get card name
+            for deck_card in player_model.deck.cards:    # Cycle cards
+                if deck_card.name == card_name:          # If card name matches
+                    # If the card matches and hasn't been used/drawn
+                    if deck_card.has_been_used is False and deck_card.in_hand is False:
+                        deck_card.in_hand = True         # Set the card to be in hand
+                        player_model.hand.extend([deck_card])
+                        print(player_model.__str__())
+                        break
+                    else:
+                        continue
 
+        ## Summon Friendly Minion
         elif "to FRIENDLY PLAY" in line:           # Friendly Minion summoned
-            print("I summoned: " + parseName(line))
+            card_name = parseName(line)
+
         elif "to OPPOSING PLAY" in line:           # Opponent minion summoned
             print("Opp summoned: " + parseName(line))
         elif "to FRIENDLY SECRET" in line:         # Secret summoned by mad scientist
             print("Secret summoned: " + parseName(line))
+    ## Card played or thrown back
     elif "from FRIENDLY HAND ->" in line:
-        if "FRIENDLY DECK" in line:                # Mulliganed card
-            print("I Mulliganed: " + parseName(line))
-        else:                                       # Any friendly card played
-            print("I Played: " + parseName(line))
+        card_name = parseName(line)
+        for deck_card in player_model.deck.cards:
+            if deck_card.name == card_name:
+                # If the card has been drawn but not used
+                if deck_card.in_hand is True and deck_card.has_been_used is False:
+                    deck_card.in_hand = False
+                    # If card is played and not returned to deck
+                    if "FRIENDLY DECK" not in line:
+                        deck_card.has_been_used = True
+        i = 0
+        for hand_card in player_model.hand:
+            if hand_card.name == card_name:
+                del player_model.hand[i]
+                break
+            i += 1
     elif "from OPPOSING HAND" in line:           # Any opponent card played
         print("Opp played: " + parseName(line))
     elif "BlockType=ATTACK" in line \
@@ -69,4 +91,3 @@ for line in loglines:
             and "Target=0" not in line \
             and "PowerTaskList.DebugPrintPower()" in line:
         print(parseName(line) + " is targeting " + parseTarget(line))
-'''
