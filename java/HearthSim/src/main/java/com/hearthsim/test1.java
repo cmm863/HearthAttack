2f534a936bd2445e8e504b80953e45b269d91fce
Kogorushi <Sepheroth89@gmail.com>
2015-11-04 15:31:02 -0600

    HearthSim stuff
    Added Diagrams to the main folder.  updated gitignore to ignore
    hearthsim build files.  modified gradle config files for our build.
    Added a HearthSim jar file to the classpath.

--- java/HearthSim/src/main/java/com/hearthsim/test1.java
+++ java/HearthSim/src/main/java/com/hearthsim/test1.java
@@ -1,7 +1,12 @@
-package com.hearthsim;
+package my.test;
+
+import com.hearthsim.card.Card;
 
 public class test1 {
-  public static void spit() {
-    // DOES NOTHING
+  public static void main(String[] args) {
+    final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(test1.class);
+    Card player = new Card();
+    log.error("HELLO WORLD?  This is not HearthSim");
   }
+
 }
