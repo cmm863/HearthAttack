18a10cc6286a837dab97fadc3e9cbd4f313e628f
Connor McBride <cmm863@mst.edu>
2015-12-08 09:52:19 -0600

    sample server py

---
+++ server.py
@@ -0,0 +1,40 @@
+__author__ = 'connor'
+# first of all import the socket library
+import socket
+from protos import deck_pb2, hero_pb2, card_pb2, player_model_pb2, weapon_pb2, minion_pb2, update_pb2
+# next create a socket object
+s = socket.socket()
+print "Socket successfully created"
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 3333
+
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('', port))
+print "socket binded to %s" %(port)
+
+# put the socket into listening mode
+s.listen(5)
+print "socket is listening"
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+   # Establish connection with client.
+   c, addr = s.accept()
+   print 'Got connection from', addr
+
+   data = c.recv(4096)
+   player_model = player_model_pb2.PlayerModel()
+   player_model.ParseFromString(data)
+   print(player_model)
+
+   # send a thank you message to the client.
+   c.send('Thank you for connecting')
+   # Close the connection with the client
+   c.close()
