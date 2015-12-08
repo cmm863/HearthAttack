00a39bd18a1a61f1dc226ea6198e0370c6aa8a63
sbwfk2 <sbwfk2@mst.edu>
2015-12-08 08:12:05 -0600

    Socket update
    Handler/Reader work over socket. Tester sends basic board model over
    socket to reader.

---
+++ java/engine/Tester.java
@@ -0,0 +1,33 @@
+//Opens a client to send default instance of boardModel message to Reader class
+
+package com.hearthattack;
+
+import com.protos.*;
+import com.hearthattack.*;
+import java.io.*;
+import java.net.Socket;
+
+public class Tester {
+  public static void main(String[] args){
+    int portNumber = Integer.parseInt(args[0]);
+    try {
+      Socket socket = new Socket("localhost", portNumber);
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      CardProto.Card card = CardProto.Card.newBuilder().setName("").build();
+      MinionProto.Minion minion = MinionProto.Minion.newBuilder().setCard(card).setTurnPlayed(0).setPosition(0).build();
+      DeckProto.Deck deck = DeckProto.Deck.newBuilder().addCards(card).build();
+      WeaponProto.Weapon weapon = WeaponProto.Weapon.newBuilder().setName("").setAttack(0).setDurability(0).build();
+      HeroProto.Hero hero = HeroProto.Hero.newBuilder().setWeapon(weapon).setArmor(0).setMinion(minion).build();
+      PlayerModelProto.PlayerModel player = PlayerModelProto.PlayerModel.newBuilder().setName("").setPlayerId(0).setHero(hero).setDeck(deck)
+                                                            .setMaxMana(0).addMinions(minion).addHand(card).build();
+      BoardModelProto.BoardModel board = BoardModelProto.BoardModel.newBuilder().setPlayer(player).setOpponent(player).build();
+      String message = board.toString();
+      out.print(message + "\n");
+      System.in.read();
+      System.out.println("Finished sending message");
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
