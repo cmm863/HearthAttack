52ea1c8f1ad4dd39f2e6f64e3de593858e09277e
gmlnnml <vilefighter23@gmail.com>
2015-12-08 20:53:04 -0600

    Eyyy so probs not work yet but work soon

--- board_eval.cc
+++ board_eval.cc
@@ -8,6 +8,15 @@
 #include "weapon.pb.h"
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>  /* define socket */
+#include <netinet/in.h>  /* define internet socket */
+#include <netdb.h>       /* define internet socket */
+
+#define SERVER_PORT 3334        /* define a server port number */
 
 using namespace std;
 
@@ -99,67 +108,11 @@ int main()
   float highscore = 0;
   int highIndex = 0;
  
-  theList.ParseFromString(msg);
-  
-  for(int i = 0; i < theList.move_size(); i++)
-  {
-    score = score_board(theList.move(i).actionboardpair(0).board());
-    if(score > highscore)
-    {
-      highscore = score;
-      highIndex = i;
-    }
-  }
-  /* Need the Action proto to be adjusted before this can be finished
-  for(int i = theList.move(highIndex).actionboardpair_length() - 1; i >= 0; i--)
-  {
-    printMove(theList.move(highIndex).actionboardpair(i).action());
-  }
-  */
-  return 0;
-}
-
-/************************************************************************/
-/*   PROGRAM NAME: server1.c  (works with client.c)                     */
-/*                                                                      */
-/*   Server creates a socket to listen for the connection from Client   */
-/*   When the communication established, Server echoes data from Client */
-/*   and writes them back.                                              */
-/*                                                                      */
-/*   Using socket() to create an endpoint for communication. It         */
-/*   returns socket descriptor. Stream socket (SOCK_STREAM) is used here*/
-/*   as opposed to a Datagram Socket (SOCK_DGRAM)                       */  
-/*   Using bind() to bind/assign a name to an unnamed socket.           */
-/*   Using listen() to listen for connections on a socket.              */
-/*   Using accept() to accept a connection on a socket. It returns      */
-/*   the descriptor for the accepted socket.                            */
-/*                                                                      */
-/*   To run this program, first compile the server_ex.c and run it      */
-/*   on a server machine. Then run the client program on another        */
-/*   machine.                                                           */
-/*                                                                      */
-/*   COMPILE:        gcc server1.c -o server1 -lnsl                     */
-/*                                                                      */
-/************************************************************************/
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>  /* define socket */
-#include <netinet/in.h>  /* define internet socket */
-#include <netdb.h>       /* define internet socket */
-
-#define SERVER_PORT 9999        /* define a server port number */
-
-int main()
-{
-    int sd, ns, k;
-    struct sockaddr_in server_addr = { AF_INET, htons( SERVER_PORT ) };
+  int sd, ns, k;
+  struct sockaddr_in server_addr = { AF_INET, htons( SERVER_PORT ) };
     struct sockaddr_in client_addr = { AF_INET };
     unsigned int client_len = sizeof( client_addr );
-    char buf[512], *host;
+    char buf[32768], *host;
 
     /* create a stream socket */
     if( ( sd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
@@ -195,12 +148,64 @@ int main()
 
     /* data transfer on connected socket ns */
     while( (k = read(ns, buf, sizeof(buf))) != 0)
-    {    printf("SERVER RECEIVED: %s\n", buf);
-         write(ns, buf, k);
+    {    
+    	printf("SERVER RECEIVED MESSAGE\n");
+    	msg = buf;
+    	
+    	theList.ParseFromString(msg);
+  
+	  for(int i = 0; i < theList.move_size(); i++)
+	  {
+	    score = score_board(theList.move(i).actionboardpair(0).board());
+	    if(score > highscore)
+	    {
+	      highscore = score;
+	      highIndex = i;
+	    }
+	  }
+	  
+	  /* Need the Action proto to be adjusted before this can be finished
+	  for(int i = theList.move(highIndex).actionboardpair_length() - 1; i >= 0; i--)
+	  {
+	    printMove(theList.move(highIndex).actionboardpair(i).action());
+	  }
+	  */
     }
     close(ns);
     close(sd);
     unlink( (const char*)&server_addr.sin_addr);
+ 
+ 
+  return 0;
+}
+
+/************************************************************************/
+/*   PROGRAM NAME: server1.c  (works with client.c)                     */
+/*                                                                      */
+/*   Server creates a socket to listen for the connection from Client   */
+/*   When the communication established, Server echoes data from Client */
+/*   and writes them back.                                              */
+/*                                                                      */
+/*   Using socket() to create an endpoint for communication. It         */
+/*   returns socket descriptor. Stream socket (SOCK_STREAM) is used here*/
+/*   as opposed to a Datagram Socket (SOCK_DGRAM)                       */  
+/*   Using bind() to bind/assign a name to an unnamed socket.           */
+/*   Using listen() to listen for connections on a socket.              */
+/*   Using accept() to accept a connection on a socket. It returns      */
+/*   the descriptor for the accepted socket.                            */
+/*                                                                      */
+/*   To run this program, first compile the server_ex.c and run it      */
+/*   on a server machine. Then run the client program on another        */
+/*   machine.                                                           */
+/*                                                                      */
+/*   COMPILE:        gcc server1.c -o server1 -lnsl                     */
+/*                                                                      */
+/************************************************************************/
+
+
+int main()
+{
+    
 
     return(0);
 }
